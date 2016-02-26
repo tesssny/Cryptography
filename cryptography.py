0a7da164143eff3ef69a0a62c75083b3e47466a0
tesssny <teresasnyder@hanovernorwichschools.org>
2016-02-26 14:22:46 -0500

    Updated from Brython Server: 2/26/2016 2:22:46 PM

--- cryptography.py
+++ cryptography.py
@@ -11,23 +11,24 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 
-start=input("Enter e to encrypt, d to decrypt, or q to quit: ")
+while start!="q":
+    start=input("Enter e to encrypt, d to decrypt, or q to quit: ")
 
 
-if start=="e" :
-    m=input("Message: ")
-    key=input("Key: ")
+    if start=="e" :
+        m=input("Message: ")
+        key=input("Key: ")
     
     
-if start=="d" :
-    m=input("Message: ")
-    key=input("Key: ")
-    print("decrypt")
+    if start=="d" :
+        m=input("Message: ")
+        key=input("Key: ")
+        print("decrypt")
     
     
-if start=="q" :
-    print("Goodbye!")
+    if start=="q" :
+        print("Goodbye!")
     
     
-if start!="q" and start!="e" and start!="d":
-    print("Did not understand command, try again.")
+    if start!="q" and start!="e" and start!="d":
+        print("Did not understand command, try again.")
